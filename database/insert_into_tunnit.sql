a1642f4e6c448c147fce0e425012b4bceb2c40d0
a1403028 <a1403028@pao5008z03.haagahelia.amk>
2016-03-08 09:59:50 +0200

    databaset

---
+++ database/insert_into_tunnit.sql
@@ -0,0 +1,6 @@
+INSERT INTO tunnit
+	(kaytID,projID,date,aloitusaika,lopetusaika,kuvaus)
+values
+	('1', '2', '2014', '18:00','20:00','tein projektia'),
+	('2', '1', '2015', '12:00','20:00','tein projektia'),
+	('2', '2', '2016', '13:00','20:00','tein projektia');
