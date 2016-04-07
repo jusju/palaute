2c6b187302c4dd9d80904587e5f42b64d3aa790d
a1403028 <a1403028@pao5008z03.haagahelia.amk>
2016-04-07 10:08:57 +0300

    dao poletti

---
+++ src/main/java/fi/tunnit/lila/dao/PolettiDAOSpringJdbcImpl.java
@@ -0,0 +1,23 @@
+package fi.tunnit.lila.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+public class PolettiDAOSpringJdbcImpl {
+	
+	
+	
+	
+
+}
