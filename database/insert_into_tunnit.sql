af551b5d52889bc0c866bbec29d7519c85753dd6
Admin <julius.porkka@gmail.com>
2016-04-18 14:28:02 +0300

    jotain

--- database/insert_into_tunnit.sql
+++ database/insert_into_tunnit.sql
@@ -1,9 +1,9 @@
 INSERT INTO tunnit
 	(kaytID,projID,date,aloitusaika,lopetusaika,kuvaus)
 values
-	('21', '1', '2014', '18:00','20:00','tein projektia'),
-	('21', '1', '2015', '12:00','20:00','tein projektia'),
-	('21', '1', '2016', '13:00','20:00','tein projektia'),
-	('21', '1', '2014', '18:00','20:00','tein projektia'),
-	('21', '1', '2015', '12:00','20:00','tein projektia'),
-	('21', '1', '2016', '13:00','20:00','tein projektia');
+	('3', '1', '2014', '18:00','20:00','tein projektia'),
+	('3', '1', '2015', '12:00','20:00','tein projektia'),
+	('3', '1', '2016', '13:00','20:00','tein projektia'),
+	('3', '1', '2014', '18:00','20:00','tein projektia'),
+	('3', '1', '2015', '12:00','20:00','tein projektia'),
+	('3', '1', '2016', '13:00','20:00','tein projektia');
